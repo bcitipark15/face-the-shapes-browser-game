012d3a0a7b73396155855bd10584b62677b9001d
Ian <bcitipark15@gmail.com>
2016-04-24 20:34:25 -0700

    test

---
+++ workspace/Test/src/p1/Test.java
@@ -0,0 +1,15 @@
+package p1;
+
+/**
+ * @author IANIK
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
