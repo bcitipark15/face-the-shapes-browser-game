8ca2dcafb904ddd8e41c63a4b1a962591523ff27
John Park <John_Park_5@hotmail.com>
2016-05-09 23:22:38 -0700

    Implemented the different screens

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
-$("#play").click(function() {
-    $('html, body').animate({ scrollTop:$("#menu").offset().top}, 500);
-});
+var colours = ["red", "green", "yellow"];
+var currentColour = 0;
+
+function hideTitle() {
+	document.getElementById("title").innerHTML="";
+}
+
+function buttonColourChange() {	
+
+	if (currentColour == colours.length - 1) {
+		currentColour = 0;
+	} else {
+		currentColour++;
+	}
+
+	document.getElementById("colour").style.backgroundColor = colours[currentColour];
+
+}
+
+function changeColour() {
+	buttonColourChange();
+}
+
+function validate() {
+
+}
+
+function random() {
+	alert("hello");
+}
