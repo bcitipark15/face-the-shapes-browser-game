6df7917e9a50f588cb5c4dfd8a1b1474fd20c57d
Ching Choi <chingchoi128@gmail.com>
2016-05-13 14:47:27 -0700

    easter egg 2 implemented onto index2 on G17 folder

--- workspace/js/foldout.js
+++ workspace/js/foldout.js
@@ -3,6 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+// Make refreshing the page to home page
+$(document).ready(function(){
+    window.location.hash = '#mainmenu';
+});
+
+//variable that determine if easter egg two is activated
+var easterEggTwoActivate = false;
+
  //Color array; added evan friendly colors
 var colors = ['red','lime','blue','purple','yellow','cyan','orange'];
 
@@ -16,6 +25,12 @@ var faceArray = [];
 var size;
 
 var colorFlag = false;
+
+//Set pivot color
+var pivotColor = 'black';
+$(document).ready(function() {
+  
+});
 
 $(document).ready(function() {
    for(var i = 0; i < faces; i++){
@@ -54,9 +69,65 @@ function makeFace(faceNum){
     return face;
 }
 
+function generateCube(){
+	for(var i = 0; i < faces; i++){
+       //Fill faceArray with new face objects
+       faceArray[i] = makeFace(i);
+	}
+   //Size of each face scales with screen to provide user with scaled faces
+   if($('#foldoutScreen').width() < $('#foldoutScreen').height()){
+	   axis = $('#foldoutScreen').width();
+   } else {
+	   axis = $('#foldoutScreen').height();
+   }
+   size = Math.floor(axis/4);
+   //Pick a face to become pivot
+	var pivot = Math.floor(Math.random() * faceArray.length);
+	//Set pivot to be full black
+
+	faceArray[pivot].trueColor = pivotColor;
+	faceArray[pivot].playerColor = pivotColor;
+	//set arrow orientation to same as pivot's
+	faceArray[pivot].value = faceArray[pivot].trueValue;
+}
+
 function foldoutT(){
 	
-	
+	if(easterEggTwoActivate === true){
+		pivotColor = 'white';
+	}
+	generateCube();
+	if (easterEggTwoActivate === true) {	
+	    $('#foldoutScreen').html('');
+	    //Create table for 2d foldout to be rendered inside of
+	    $('#foldoutScreen').append('<table id="foldout"></table>');
+	    
+	    //Fill table with faces
+	    for(var i = 0; i < faces; i++){
+	        if(i === 1 || i === 2 || i === 3){
+	            //Appends div containing foldoutFace 1 2 or 3 to the current table column
+	            $("#foldout").append('<td>' +
+	                                 '<div class="foldoutFace" id="foldoutFace' + i +
+	                                 '" onclick="rotateFace(\'foldoutFace' + i + '\')"' +
+	                                 '></div></td>');
+	        } else {
+	            $("#foldout").append('<tr><td></td><td>' +
+	                                 '<div class="foldoutFace" id="foldoutFace' + i +
+	                                 '" onclick="rotateFace(\'foldoutFace' + i + '\')"' +
+	                                 '></div></td></tr>');
+	        }
+	        $('#foldoutFace' + i).append('<img src="./workspace/image/skull.png" height="50%"' +
+	                                     '" width="50%">');
+	        $('#foldoutFace' + i).css('transform', 'rotateZ(' + faceArray[i].value * 90 + 'deg)');
+	        
+	        
+	        $('#foldoutFace' + i).css('background-color', faceArray[i].playerColor);
+	    }
+	    $('.foldoutFace').css({'width': size, 'height': size, 'border': 'solid 1px black'});
+	    $('.foldoutFace').children().css({'position':'absolute','left':'0','right':'0','bottom':'0','top':'0','margin':'auto'})
+
+	} else {
+
     $('#foldoutScreen').html('');
     //Create table for 2d foldout to be rendered inside of
     $('#foldoutScreen').append('<table id="foldout"></table>');
@@ -83,6 +154,7 @@ function foldoutT(){
         $('#foldoutFace' + i).css('background-color', faceArray[i].playerColor);
     }
     $('.foldoutFace').css({'width': size, 'height': size, 'border': 'solid 1px black'});
+	}
 }
 /**
  * rotateFace Rotates the image of the face that was clicked on.
@@ -134,4 +206,26 @@ function validate(){
 		$('#resultMessage').append('You got a face wrong!!');
 	}
 	$('#correctAnswer').append(correct1 + '<br>' + correct2);
+}
+
+//Format to easter egg two style once called.
+function easterEggTwo() {
+	easterEggTwoActivate = true;
+	var easterEggTwoActivateHead = document.getElementsByTagName('head').item(0);
+	var easterEggTwoActivateStyle = document.createElement("style");
+	easterEggTwoActivateStyle.type = "text/css"; 
+	easterEggTwoActivateStyle.appendChild(document.createTextNode("#gameContainer{background-color: #042714;"
+			+ "background-image: linear-gradient(120deg, #021127, #042714);} "
+			+ "body{background-color:black;}"
+			+ ".title{color: rgba(200, 100, 100, 0.7);}"
+			+ ".buttons{color: rgba(200, 100, 100, 0.7);}"
+			+ "#cube img{width: 50%; height: 50%;}"));
+	easterEggTwoActivateHead.appendChild(easterEggTwoActivateStyle);
+	var easterEggTwoActivateCubeFace = document.getElementById('cube');
+	easterEggTwoActivateCubeFace.innerHTML = '<figure class="front"><img class="cubeCover" src="./workspace/image/skull.png" alt="arrow"></figure> '
+            + '<figure class="back"><img class="cubeCover" src="./workspace/image/skull.png" alt="arrow"></figure>'
+            + '<figure class="right"><img class="cubeCover" src="./workspace/image/skull.png" alt="arrow"></figure>'
+            + '<figure class="left"><img class="cubeCover" src="./workspace/image/skull.png" alt="arrow"></figure>'
+            + '<figure class="top"><img class="cubeCover" src="./workspace/image/skull.png" alt="arrow"></figure>'
+            + '<figure class="bottom"><img class="cubeCover" src="./workspace/image/skull.png" alt="arrow"></figure>'
 }
