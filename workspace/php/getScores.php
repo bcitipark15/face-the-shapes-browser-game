be2ed1b4a816b9e72c8a81cf4e932e878b51e534
John Park <John_Park_5@hotmail.com>
2016-05-17 22:05:41 -0700

    Added some code to php

--- workspace/php/getScores.php
+++ workspace/php/getScores.php
@@ -1,6 +1,6 @@
 <?php 
 
-$con =  mysqli_connect('mysql6.000webhost.com','a4492753_g17fts','parkian93') or 
+$con = new mysqli_connect('mysql6.000webhost.com','a4492753_g17fts','parkian93') or 
 	die(mysqli_error($con));
 
 mysqli_select_db($con, 'a4492753_g17fts') or 
@@ -9,4 +9,11 @@ mysqli_select_db($con, 'a4492753_g17fts') or
 $query = "SELECT * FROM scores ORDER BY time_score DESC LIMIT 10"
 $info = mysqli_query($con, $query);
 
-while($row = mysqli_fetch_array)
+while($row = mysqli_fetch_assoc($result)) {
+	echo "<tr>";
+	echo "<td>" . $row['rank'] . "</td>";
+	echo "<td>" . $row['name'] . "</td>";
+	echo "<td>" . $row['highScore'] . "</td>";
+	echo "<td>" . $row['date'] . "</td>";
+	echo "</tr>";
+}
--- workspace/php/inputScore.php
+++ workspace/php/inputScore.php
@@ -3,7 +3,7 @@
 $name = ($_GET['name']);
 $score = ($_GET['score']);
 
-$con =  mysqli_connect('mysql6.000webhost.com','a4492753_g17fts','parkian93') or 
+$con = new mysqli_connect('mysql6.000webhost.com','a4492753_g17fts','parkian93') or 
 	die(mysqli_error($con));
 
 mysqli_select_db($con, 'a4492753_g17fts') or 
