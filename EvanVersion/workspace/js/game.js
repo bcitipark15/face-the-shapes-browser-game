9bed3ff31e0b7671a3801139c6b5baa3fb22f92e
Evan <evan.c1995@gmail.com>
2016-05-18 20:00:33 -0700

    Changed level select

--- EvanVersion/workspace/js/game.js
+++ EvanVersion/workspace/js/game.js
@@ -1,3 +1,4 @@
+/* The easter egg */
 var count = 0;
 function tutturu(){
     var audio = new Audio('./workspace/audio/tuturu-century-fox.mp3');
@@ -8,22 +9,21 @@ function tutturu(){
 	}
 	
 }
+/*window.setTimeout(scrollDown, 100);*/
 
-setInterval(removeNavBar, 10) 
 
-function removeNavBar() {
-	/*window.scrollTo(0, 1);*/
+/* Calculate the available screen real estate without the address bar and set it as the height  */
+function navBar() {
+	var a = screen.height;
+	var b = window.innerHeight;
+	var difference =  a - ( (a - b) / 4 ) ;	
+	$('#gameContainer').css('height', difference);
+	
 }
 
-
+/* The screen changing function */
 function screenChange(current, next) {
 	document.getElementById(next).style.display = "block";
 	document.getElementById(current).style.display = "none";
 }
-/*
-window.addEventListener("orientationchange", function() {
-	if (window.innerWidth > window.innerHeight) {
-		$('body').css('transform', 'rotate(90deg)');
-	}
-}, false);
-*/
+
