942fe922cdbef89e65b256ef570a4ed5505f2f50
Ian Park <bcitipark15@gmail.com>
2016-05-19 14:35:36 -0700

    some changes

--- workspace/php/inputScore.php
+++ workspace/php/inputScore.php
@@ -1,7 +1,7 @@
 <?php 
 
 $name = ($_GET['name']);
-$name = mysql_real_escape($name);
+/*$name = mysqli_real_escape_string('$name');*/
 $score = intval($_GET['score']);
 
 $con = mysqli_connect('localhost','ftsg17','parkian93') or 
@@ -10,7 +10,7 @@ $con = mysqli_connect('localhost','ftsg17','parkian93') or
 mysqli_select_db($con, 'ftsg17_FaceTheShapes') or 
 	die(mysqli_error($con));
 
-$query = "INSERT INTO scores(name, score) VALUES('$name', $score)";
+$query = "INSERT INTO scores(name, score) VALUES('$name', '$score')";
 
 mysqli_query($con, $query);
 
