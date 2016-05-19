00a3bfa70b3692dd18725bb831d3fedab9dd10a1
John <john_park_5@hotmail.com>
2016-05-19 10:39:43 -0700

    Finished php and ajax

--- workspace/php/getScores.php
+++ workspace/php/getScores.php
@@ -1,12 +1,21 @@
 <?php 
 
-$con =  mysqli_connect('mysql6.000webhost.com','a4492753_g17fts','parkian93') or 
+$con =  mysqli_connect('localhost','group17','parkian93') or 
 	die(mysqli_error($con));
 
-mysqli_select_db($con, 'a4492753_g17fts') or 
+mysqli_select_db($con, 'group17_FaceTheShapes') or 
 	die(mysqli_error($con));
 
 $query = "SELECT * FROM scores ORDER BY time_score DESC LIMIT 10"
 $info = mysqli_query($con, $query);
 
-while($row = mysqli_fetch_array)
+while($row = mysqli_fetch_array($info)) {
+	echo "<tr>";
+	echo "<td>" . $row['rank'] . "</td>";
+	echo "<td>" . $row['name'] . "</td>";
+	echo "<td>" . $row['score'] . "</td>";
+	echo "<td>" . $row['date'] . "</td>";
+	echo "</tr>";
+}
+
+mysqli_close($con);
