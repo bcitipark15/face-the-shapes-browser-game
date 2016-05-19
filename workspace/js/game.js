c676f58420f82be8f77caa6c50c74d6d82bf1868
Evan <evan.c1995@gmail.com>
2016-05-19 15:15:20 -0700

    Fixed screen size orientation and improved the look of the input box

--- workspace/js/game.js
+++ workspace/js/game.js
@@ -9,21 +9,18 @@ function tutturu(){
 	
 }
 
-setInterval(removeNavBar, 10) 
-
-function removeNavBar() {
-	/*window.scrollTo(0, 1);*/
+/* Calculate the available screen real estate without the address bar and set it as the height  */
+function navBar() {
+	var a = screen.height;
+	var b = window.innerHeight;
+	var difference =  a - ( (a - b) / 4 ) ;
+	
+	$('#gameContainer').css('height', Math.round(difference));
+	
 }
-
-
 function screenChange(current, next) {
 	document.getElementById(next).style.display = "block";
 	document.getElementById(current).style.display = "none";
 }
-/*
-window.addEventListener("orientationchange", function() {
-	if (window.innerWidth > window.innerHeight) {
-		$('body').css('transform', 'rotate(90deg)');
-	}
-}, false);
-*/
+
+window.addEventListener("orientationchange", navBar, false);
