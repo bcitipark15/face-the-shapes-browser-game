4bb5cb359cbd602e1dd4d12866bdf591b8565f32
Ian Park <bcitipark15@gmail.com>
2016-05-20 15:30:24 -0700

    Commented

--- workspace/php/inputScore.php
+++ workspace/php/inputScore.php
@@ -1,17 +1,24 @@
 <?php 
 
+// Get the name of the player.
 $name = ($_GET['name']);
-/*$name = mysqli_real_escape_string('$name');*/
+
+// Get the score of the player.
 $score = intval($_GET['score']);
 
+// Try to connect to the database server.
 $con = mysqli_connect('localhost','ftsg17','parkian93') or 
 	die(mysqli_error($con));
-	
+
+// Try to open the database.
 mysqli_select_db($con, 'ftsg17_FaceTheShapes') or 
 	die(mysqli_error($con));
 
+// Insert the name and the score of the player's into the database.
 $query = "INSERT INTO scores(name, score) VALUES('$name', '$score')";
 
+// Perform the query above.
 mysqli_query($con, $query);
 
+// Close the connection to the database.
 mysqli_close($con);
