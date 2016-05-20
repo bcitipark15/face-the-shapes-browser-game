a52fa708c48b6adacbfd6dd8b6dc2b2a0ae83a3a
Ian Park <bcitipark15@gmail.com>
2016-05-20 15:33:24 -0700

    Changed variable name for the query

--- workspace/php/getScores.php
+++ workspace/php/getScores.php
@@ -11,11 +11,11 @@ $connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect t
 
 // Change the rows according to the scores in descending order and limit the
 // number of players to display to top 10.
-$test_query = 'SELECT rank, name, score FROM scores ORDER BY score DESC LIMIT 10';
+$query = 'SELECT rank, name, score FROM scores ORDER BY score DESC LIMIT 10';
 
 // Try to open the database.
 mysqli_select_db($connect, $dbname) or die("Could not open the db '$dbname'");
-$result = mysqli_query($connect, $test_query);
+$result = mysqli_query($connect, $query);
 
 // Get all the rows from the database and put them on the leaderboard table.
 while ($row = mysqli_fetch_array($result, true)) {
