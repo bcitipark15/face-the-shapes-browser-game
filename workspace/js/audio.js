53f89632667e13ace1d0d83eb8d3bd2d3adf8a7b
John Park <John_Park_5@hotmail.com>
2016-05-23 12:19:58 -0700

    Added audio settings

--- workspace/js/audio.js
+++ workspace/js/audio.js
@@ -1,4 +1,35 @@
+var state = 0;
+
 function buttonClick() {
 	var click = document.getElementById("buttonSound");
     click.play();
+}
+
+function mute() {
+	document.getElementById("bgm").muted = true;
+	document.getElementById("buttonSound").muted = true;
+}
+
+function unmute() {
+	document.getElementById("bgm").muted = false;
+	document.getElementById("buttonSound").muted = false;
+}
+
+function volume(value) {
+	document.getElementById("bgm").volume = value;
+	document.getElementById("buttonSound").volume = value;
+}
+
+function muteButton() {
+	if (state == 0) {
+		mute();
+		document.getElementById(muteToggle).innerhtml = "Unmute";
+		state++;
+	} 
+
+	if (state == 1) {
+		unmute();
+		document.getElementById(muteToggle).innerhtml = "Mute";
+		state--;
+	}
 }
