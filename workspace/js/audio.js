fd9eaddcabfb4b7c09a1243efe5e42059448283c
John Park <John_Park_5@hotmail.com>
2016-05-25 21:27:30 -0700

    Modified audio, and achievements

--- workspace/js/audio.js
+++ workspace/js/audio.js
@@ -23,16 +23,39 @@ function unmute() {
 	document.getElementById("buttonSound").muted = false;
 }
 
-function volume(value) {
-	document.getElementById("bgm").volume = value;
-	document.getElementById("buttonSound").volume = value;
+// function volume(value) {
+// 	document.getElementById("bgm").volume = value;
+// 	document.getElementById("buttonSound").volume = value;
+// }
+
+var volumeVal = 0.5;
+function volumeUp() {
+	if (volumeVal >= 1) {
+		volumeVal = 1;
+	} else {
+		volumeVal += 0.1
+		document.getElementById("bgm").volume = (volumeVal);
+		document.getElementById("buttonSound").volume = (volumeVal);
+	}
+	document.getElementById("volumePercent").innerHTML=(volumeVal * 100).toFixed() + "%";
+}
+
+function volumeDown() {
+	if (volumeVal <= 0) {
+		volumeVal = 0;
+	} else {
+		volumeVal -= 0.1
+		document.getElementById("bgm").volume = (volumeVal);
+		document.getElementById("buttonSound").volume = (volumeVal);
+	}
+	document.getElementById("volumePercent").innerHTML=(volumeVal * 100).toFixed() + "%";
 }
 
 var state = 0;
 function muteButton() {
 	if (state == 0) {
 		mute();
-		document.getElementById(muteToggle).innerhtml = "Unmute";
+		document.getElementById(muteButton).innerhtml = "Unmute";
 		state++;
 	} else if (state == 1) {
 		unmute();
