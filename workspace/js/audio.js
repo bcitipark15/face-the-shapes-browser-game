3260a4ab126c95a7a8884d010c2df2afd0a7c487
John Park <John_Park_5@hotmail.com>
2016-05-26 22:53:26 -0700

    modified audio js

--- workspace/js/audio.js
+++ workspace/js/audio.js
@@ -1,38 +1,53 @@
+/* 
+ * Author: Team 17 (Chris Centenera, Evan Chen, Ching Choi, John Park, Ian Park)
+ * Description: JavaScript file that handles all the audio settings and calls in 
+ * 				the game.
+ */
+
+//Plays the sound for buttons.
 function buttonClick() {
 	var click = document.getElementById("buttonSound");
     click.play();
 }
 
+//Plays the sound for correct answer.
 function correctSound() {
 	var correct = document.getElementById("correctTada");
     correct.play();
 }
 
+//Plays the sound for incorrect answer.
 function incorrectSound() {
 	var wrong = document.getElementById("incorrectDoh");
     wrong.play();
 }
 
+//Plays the sound for unlocked achievement.
 function achievementSound() {
 	var unlock = document.getElementById("achievementMagic");
     unlock.play();
 }
 
+//Mutes all the audio.
 function mute() {
 	document.getElementById("bgm").muted = true;
 	document.getElementById("buttonSound").muted = true;
+	document.getElementById("correctTada").muted = true;
+	document.getElementById("incorrectDoh").muted = true;
+	document.getElementById("achievementMagic").muted = true;
 }
 
+
+//Unmutes all the audio.
 function unmute() {
 	document.getElementById("bgm").muted = false;
 	document.getElementById("buttonSound").muted = false;
+	document.getElementById("correctTada").muted = true;
+	document.getElementById("incorrectDoh").muted = true;
+	document.getElementById("achievementMagic").muted = true;
 }
 
-// function volume(value) {
-// 	document.getElementById("bgm").volume = value;
-// 	document.getElementById("buttonSound").volume = value;
-// }
-
+//Function to increase the volume by 10 percent.
 var volumeVal = 0.5;
 function volumeUp() {
 	if (volumeVal >= 1) {
@@ -45,6 +60,7 @@ function volumeUp() {
 	document.getElementById("volumePercent").innerHTML=(volumeVal * 100).toFixed() + "%";
 }
 
+//Function to decrease the volume by 10 percent.
 function volumeDown() {
 	if (volumeVal <= 0) {
 		volumeVal = 0;
@@ -56,7 +72,10 @@ function volumeDown() {
 	document.getElementById("volumePercent").innerHTML=(volumeVal * 100).toFixed() + "%";
 }
 
+//Variable representing the state of audio. (muted or not)
 var state = 0;
+
+//Mutes & unmutes audio and renames the mute toggle button.
 function muteButton() {
 	if (state == 0) {
 		mute();
