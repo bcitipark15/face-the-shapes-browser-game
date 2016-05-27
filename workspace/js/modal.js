8a3a44ba26009748e0b73c4df7e5f4dcf967d477
Christopher Centenera <Christopher_Centenera@hotmail.com>
2016-05-26 22:00:12 -0700

    many fixes

--- workspace/js/modal.js
+++ workspace/js/modal.js
@@ -174,12 +174,32 @@ function compDiffSelectButtonChange(){
 	}
 }
 
-function achievementModal(get) {
+function achievementModal(achivementNum) {
 	$('.modalContent').html('');
-	$('.modalContent').append('<h1>Achievement Get</h1>'
+	var achivementName;
+	var achievementImage;
+	var achivementText;
+	switch(achivementNum){
+		case 1:
+			var achievementName = '<h1>The First Step</h1>';
+			var achievementImage = '<li><img src="./workspace/image/Quas.png" alt="achievement1" width="50%" height="50%"></li>';
+			var achievementText = '<li><p>Solve your first cube</p></li>';
+			break;
+		case 2:
+			var achievementName = '<h1>The First Step</h1>';
+			var achievementImage = '<li><img src="./workspace/image/Quas.png" alt="achievement1" width="50%" height="50%"></li>';
+			var achievementText = '<li><p>Solve your first cube</p></li>';
+			break;
+		case 3:
+			var achievementName = '<h1>The First Step</h1>';
+			var achievementImage = '<li><img src="./workspace/image/Quas.png" alt="achievement1" width="50%" height="50%"></li>';
+			var achievementText = '<li><p>Solve your first cube</p></li>';
+			break;
+	}
+	$('.modalContent').append(achievementName
 							+	'<ol>' 
-							+		'<li><img src="something"></li>'
-							+		'<li><p>Text for something</p></li>'
+							+		achievementImage
+							+		achievementText
 							+		'<li><button class="buttonDesign menuBSize" onclick="hideModal();">Okay</button></li>'
 							+	'</ol>');
 	displayModal();
