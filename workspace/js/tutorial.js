52799feb98fa38e48f31d2abd83c2e424abdf8ee
Ching Choi <chingchoi128@gmail.com>
2016-05-26 22:00:01 -0700

    merging current tutorials material with chris

--- workspace/js/tutorial.js
+++ workspace/js/tutorial.js
@@ -183,7 +183,7 @@ function timeHelper(page) {
 				'margin'	: "auto"
 			});
 			$('#timerBox').css({
-				'z-index' 	: '4',
+				'z-index' 	: '2',
 				'position' 	: 'absolute',
 				'zoom'		: '0.8',
 				'bottom'	: '240px'
@@ -195,7 +195,6 @@ function timeHelper(page) {
 			break;
 			
 		case 2:
-			
 			$('#timerBox').css({
 				'z-index' 	: '0',
 				'position' 	: 'relative',
@@ -228,6 +227,119 @@ function timeHelper(page) {
 }
 
 /**
+ * Score attack mode tutorial screen activator.
+ * @param {int} indicate which page is currently being referred to.
+ */
+function scoreHelper(page) {
+	helperMode = 3;
+	switch(page){
+		case 0:
+			$('#helpContainer').css('display', 'initial');
+			$('#overlay').css('display', 'initial');
+			$('#scoreHelp1').css('display', 'initial');
+			$('#scoreHelp2').css('display', 'none');
+			$('#proceed').css('display', 'initial');
+			$('#preceed').css('display', 'none');
+			$('#cubeContainer').css({
+				'z-index' 	: '0',
+				'position'	: 'relative',
+				'left'		: '0',
+				'right'		: '0', 
+				'margin'	: "auto"
+			});
+			$('#timerBox').css({
+				'z-index' 	: '0',
+				'position' 	: 'relative',
+				'zoom'		: '1',
+				'bottom'	: '0'
+			});			
+			$('#scoreBox').css({
+				'z-index' 	: '0',
+				'position' 	: 'relative',
+				'zoom'		: '1',
+				'right'		: '0', 
+				'bottom'	: '0'
+			});	
+			break;
+
+		case 1:
+			$('#cubeContainer').css({
+				'z-index' 	: '2',
+				'position'	: 'absolute',
+				'left'		: '0',
+				'right'		: '0', 
+				'margin'	: "auto"
+			});
+			$('#timerBox').css({
+				'z-index' 	: '2',
+				'position' 	: 'absolute',
+				'zoom'		: '0.8',
+				'bottom'	: '240px'
+			});		
+			$('#scoreBox').css({
+				'z-index' 	: '2',
+				'position' 	: 'absolute',
+				'zoom'		: '0.8',
+				'right'		: "0", 
+				'bottom'	: '240px'
+			});	
+			$('#scoreHelp1').css('display', 'none');
+			$('#scoreHelp2').css('display', 'initial');
+			$('#scoreHelp3').css('display', 'none');
+			$('#preceed').css('display', 'initial');
+			break;
+			
+		case 2:
+			
+			$('#timerBox').css({
+				'z-index' 	: '0',
+				'position' 	: 'relative',
+				'zoom'		: '1',
+				'bottom'	: '0'
+			});			
+			$('#cubeContainer').css({
+				'z-index' 	: '0',
+				'position'	: 'relative',
+				'left'		: '0',
+				'right'		: '0', 
+				'margin'	: "auto"
+			});			
+			$('#scoreBox').css({
+				'z-index' 	: '0',
+				'position' 	: 'relative',
+				'zoom'		: '1',
+				'right'		: '0', 
+				'bottom'	: '0'
+			});	
+			$('#scoreHelp2').css('display', 'none');
+			$('#scoreHelp3').css('display', 'initial');
+			$('#proceed').css('display', 'none');
+			$('#endHelp').css('display', 'initial');
+			break;
+		case 4:
+			screenChange('gameMode');
+			$('#classicHelp5').css('display', 'none');
+			$('#helpContainer').css('display', 'none');
+			$('#overlay').css('display', 'none');
+			$('#proceed').css('display', 'none');
+			$('#endHelp').css('display', 'none');
+			currentPage = 0;
+			helperMode = 0;
+			break;			
+		case 4:
+			screenChange('gameMode');
+			$('#classicHelp5').css('display', 'none');
+			$('#helpContainer').css('display', 'none');
+			$('#overlay').css('display', 'none');
+			$('#proceed').css('display', 'none');
+			$('#endHelp').css('display', 'none');
+			currentPage = 0;
+			helperMode = 0;
+			break;
+		}
+}
+
+/**
  * Transition to next page of the tutorial.
  */
 
@@ -240,6 +352,9 @@ function nextPage(direction) {
 		if (helperMode == 2) {
 			timeHelper(currentPage);
 		}
+		if (helperMode == 3) {
+			scoreHelper(currentPage);
+		}
 	} else if (direction == 'previous') {
 		currentPage--;
 		if (helperMode == 1) {
@@ -247,6 +362,9 @@ function nextPage(direction) {
 		}
 		if (helperMode == 2) {
 			timeHelper(currentPage);
+		}
+		if (helperMode == 3) {
+			scoreHelper(currentPage);
 		}
 	}
 }
